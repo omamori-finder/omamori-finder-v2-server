a99a509cf6a7ff493e81dcc86092fca0d5dfb066
Anissa3005 <anissa.chadouli@gmail.com>
2024-09-20 17:06:43 +0900

    add missing fields to the schema

--- src/schemas/omamori.py
+++ src/schemas/omamori.py
@@ -1,11 +1,14 @@
 from pydantic import BaseModel, Field, UUID4
 from datetime import datetime
+from enum import Enum
+from enum_types import PrefectureEnum
 
 
 class OmamoriInput(BaseModel):
     shrine_name: str = Field(examples=['Meiji Jingu'])
     google_maps_link: str = Field(
         examples=['https://maps.app.goo.gl/RAAtiAsSBkA5X2UM6'])
+    prefecture: PrefectureEnum
     description: str | None = None
     photo_url: str = Field(examples=['url/something'])
 
