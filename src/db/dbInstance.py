167492d2c86d63c492eae72cbd28441730bd4a2a
Anissa3005 <anissa.chadouli@gmail.com>
2024-12-02 15:53:06 +0900

    Change the keys in in the boto3.resource

--- src/db/dbInstance.py
+++ src/db/dbInstance.py
@@ -1,5 +1,4 @@
 import boto3
-import botocore.exceptions
 from botocore.config import Config
 import logging
 
@@ -10,43 +9,7 @@ logger = logging.getLogger(__name__)
 dynamodb = boto3.resource(
     "dynamodb",
     endpoint_url="http://host.docker.internal:8000",
-    aws_access_key_id="testid",
-    aws_secret_access_key="mysecret",
+    aws_access_key_id="X",
+    aws_secret_access_key="X",
     region_name="localhost"
 )
-
-
-def create_table(table_name: str):
-    try:
-        omamori_table = dynamodb.create_table(
-            TableName=table_name,
-            KeySchema=[
-                {
-                    "AttributeName": "uuid",
-                    "KeyType": "HASH"
-                }
-            ],
-            AttributeDefinitions=[
-                {
-                    "AttributeName": "uuid",
-                    "AttributeType": "S"
-                },
-            ],
-            ProvisionedThroughput={
-                "ReadCapacityUnits": 5,
-                "WriteCapacityUnits": 5
-            }
-        )
-
-        print(table_name, "is",
-              omamori_table)
-    except botocore.exceptions.ClientError as err:
-        print("Couldn't create table %s. Here's why: %s: %s",
-              table_name,
-              err.response["Error"]["Code"],
-              err.response["Error"]["Message"]
-              )
-        raise
-
-
-# create_table('omamori')
