4b3ffc6c51c4fd72404da11a9ebb5980f770f936
Anissa3005 <anissa.chadouli@gmail.com>
2024-12-03 16:21:00 +0900

    refactor upload_omamori_picture with correct type hinting and error handleing

--- src/db/s3.py
+++ src/db/s3.py
@@ -1,23 +1,25 @@
 import boto3
 import logging
 import uuid
-from botocore.exceptions import ClientError
+from botocore.exceptions import ClientError, BotoCoreError
+from src.custom_error import CustomException
+from src.custom_error import ErrorCode
 
 logger = logging.getLogger(__name__)
 
 
-def upload_picture(file, bucket='omamori-finder-pictures'):
+def upload_picture(file, bucket='omamori-finder-pictures') -> str:
     try:
         client = boto3.client('s3')
         object_name = f'media/{str(uuid.uuid4())}.jpg'
         client.upload_fileobj(file.file, bucket, object_name)
-    # TO DO: Raise custom error here
-    except ClientError as err:
+        return object_name
+    except (ClientError, BotoCoreError) as err:
         logging.error("Was not able to upload the picture to S3 bucket",
                       "Here is why",
                       err)
-        return False
-    return object_name
+        raise CustomException(field="upload_picture",
+                              error_code=ErrorCode.SERVER_ERROR, status_code=500)
 
 
 def delete_picture_by_object_name(object_name: str):
