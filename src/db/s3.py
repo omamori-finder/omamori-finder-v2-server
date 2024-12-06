e86820c852a8bebf155f34954a29c38421e6809e
Anissa3005 <anissa.chadouli@gmail.com>
2024-12-06 14:10:07 +0900

    Change the s3 bucket name to development buckets name

--- src/db/s3.py
+++ src/db/s3.py
@@ -8,7 +8,7 @@ from src.custom_error import ErrorCode
 logger = logging.getLogger(__name__)
 
 
-def upload_picture(file, bucket='omamori-finder-pictures') -> str:
+def upload_picture(file, bucket='omamori-finder-pictures-development') -> str:
     try:
         client = boto3.client('s3')
         object_name = f'media/{str(uuid.uuid4())}.jpg'
@@ -26,7 +26,7 @@ def delete_picture_by_object_name(object_name: str):
     client = boto3.client("s3")
     try:
         response = client.delete_object(
-            Bucket="omamori-finder-pictures", Key=object_name)
+            Bucket="omamori-finder-pictures-development", Key=object_name)
         return response
     except ClientError as err:
         logging.error(err)
