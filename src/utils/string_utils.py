b92ba5c9683902a1fa25f7f8f54c5e6ecccb14c5
Anissa3005 <anissa.chadouli@gmail.com>
2024-12-12 13:46:24 +0900

    fix regex that checks strings

--- src/utils/string_utils.py
+++ src/utils/string_utils.py
@@ -2,7 +2,7 @@ import re
 
 
 def has_special_characters(string_value: str) -> bool:
-    regex_special_characters = r"[~`!#$%\^&*+={}()\-\[\]\\']+"
+    regex_special_characters = r"[~`?!#$%\^&*+={}()\-\[\]\\']+"
     contains_invalid_character = re.search(
         regex_special_characters, string_value)
 
@@ -13,8 +13,9 @@ def has_special_characters(string_value: str) -> bool:
 
 
 def has_script_tags(string_value: str) -> bool:
-    regex_script_tags = r"/<script.*?>.*?<\/script>/gi"
-    contains_script_tags = re.search(regex_script_tags, string_value)
+    regex_script_tags = r"<script\b.*?>.*?</script>"
+    contains_script_tags = re.search(
+        regex_script_tags, string_value, re.IGNORECASE)
 
     if contains_script_tags != None:
         return True
